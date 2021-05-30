791b40f62881d6bdc483dd832f84ec7bc232201f
Postroggy <2451527090@qq.com>
2021-05-30 12:43:22 +0800

    dev version

--- start.sh
+++ start.sh
@@ -4,16 +4,14 @@ mkdir Bin/Darwin
 
 # compile windows version
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./Bin/Windows/TCPchatClient main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./Bin/Windows/TerminalClient ./ChatClient/Client.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o ./Bin/Windows/TerminalClient ./ConsoleClient/terminalClient.go
 # compile linux version
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./Bin/Linux/TCPchatClient main.go
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./Bin/Linux/TerminalClient ./ChatClient/Client.go
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o ./Bin/Linux/TerminalClient ./ConsoleClient/terminalClient.go
 # compile darwin version
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./Bin/Darwin/TCPchatClient main.go
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./Bin/Darwin/TerminalClient ./ChatClient/Client.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o ./Bin/Darwin/TerminalClient ./ConsoleClient/terminalClient.go
 
-go build -o TCPchatRoom
 PID=$(lsof -t -i:8888)
 echo $PID
 kill -9 $PID
-./TCPchatRoom
